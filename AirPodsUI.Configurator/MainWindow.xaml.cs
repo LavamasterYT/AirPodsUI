9265a1765917bf644b70752efc2b01ce9a9e50cb
LavamasterYT <48543854+LavamasterYT@users.noreply.github.com>
2020-06-22 21:36:28 -0500

    Add WPF project and dark/light color schemes

---
+++ AirPodsUI.Configurator/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using AdonisUI;
+using AdonisUI.Controls;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AirPodsUI.Configurator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : AdonisWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+            if (int.TryParse(reg.GetValue("AppsUseLightTheme").ToString(), out int result))
+            {
+                if (result == 0)
+                {
+                    TitleBarBackground = System.Windows.Media.Brushes.Black;
+                    ResourceLocator.SetColorScheme(Application.Current.Resources, ResourceLocator.DarkColorScheme);
+                }
+                else
+                {
+                    TitleBarBackground = System.Windows.Media.Brushes.White;
+                    ResourceLocator.SetColorScheme(Application.Current.Resources, ResourceLocator.LightColorScheme);
+                }
+            }
+        }
+    }
+}
